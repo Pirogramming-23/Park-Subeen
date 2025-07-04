e443c904b83acadc176a5abb8155ced85af0f5df
subeeen <rose9165676@naver.com>
2025-07-04 12:20:27 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,19 +1,25 @@
+import random
+
 def brGame():
     num=0
-    turn = "playerA"
+    turn = "computer"
 
     while num < 31:
-        while True:
-            try:
-                count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-                if count not in [1, 2, 3]:
-                    print("1,2,3 중 하나를 입력하세요")
-                else:
-                    break
-            except ValueError:
-                print("정수를 입력하세요")
+        if turn == "computer":
+            count = random.randint(1, 3)
+            print(f"computer {count}")
+        else:
+            while True:
+                try:
+                    count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+                    if count not in [1, 2, 3]:
+                        print("1,2,3 중 하나를 입력하세요")
+                    else:
+                        break
+                except ValueError:
+                    print("정수를 입력하세요")
 
-        for i in range(1, count + 1):
+        for i in range(count):
             num += 1
             print(f"{turn} : {num}")
             if num >= 31:
@@ -22,7 +28,7 @@ def brGame():
         if num >= 31:
             break
 
-        turn = "playerB" if turn == "playerA" else "playerA"
+        turn = "player" if turn == "computer" else "computer"
 
     print(f"{turn} win!")
 
